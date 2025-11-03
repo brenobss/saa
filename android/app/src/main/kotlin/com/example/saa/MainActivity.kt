03ab8c84bd52db00330044fc9371f39a921e2d98
Breno <brenobarreto5@hotmail.com>
2025-11-03 13:30:03 -0300

    init flutter project and create main

---
+++ android/app/src/main/kotlin/com/example/saa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.saa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+void main() {
+  runApp(const SAAApp());
+}
+
+class SAAApp extends StatelessWidget {
+  const SAAApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SAA',
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      home: const LoginScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
